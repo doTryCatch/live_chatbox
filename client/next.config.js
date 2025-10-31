f2159e3848247bdacdc08fe342eb25910305d190
Roshan kumar Patel <142009973+doTryCatch@users.noreply.github.com>
2025-10-31 17:55:13 +0530

    Add basePath and assetPrefix to next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'live_chatbox'; // 👈 your GitHub repo name here
+
 const nextConfig = {
-    output: 'export', // ✅ replaces "next export"
-    images: {
-      unoptimized: true, // ✅ disable optimization for static export
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
